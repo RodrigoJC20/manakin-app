f03e929d7af7cb597ccb46fcb12a30da7cc3d53f
Pauline Wee <paulinekowee@gmail.com>
2024-06-01 12:42:02 -0400

    Ss

--- manakin/app/page.tsx
+++ manakin/app/page.tsx
@@ -14,20 +14,31 @@ export default function Home() {
       {/* <h1 className="text-4xl font-bold">Welcome to Manakin</h1> */}
       <div className="flex flex-col items-center">
         <button
+          className="button"
           style={{
-            height: '260px', // 3 times the screen height
-            width: '800px', // 3 times the screen height
-            marginBottom: '20px',
+            height: '340px',
+            width: '840px',
+            position: 'absolute',
+            left: '100px',
+            bottom: '-1580px',
             backgroundImage: 'url(img/button1.png)',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}
-        >
-          Upload wooo
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Upload background
-        </button>
+        ></button>
+        <button
+          className="button"
+          style={{
+            height: '330px',
+            width: '870px',
+            position: 'absolute',
+            left: '450px',
+            bottom: '-1880px',
+            backgroundImage: 'url(img/button2.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        ></button>
       </div>
     </main>
   )
