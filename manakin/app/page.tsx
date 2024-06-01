a3d234431e91c1a0242029c638346b3fddda09df
Pauline Wee <paulinekowee@gmail.com>
2024-06-01 12:19:42 -0400

    landing page

--- manakin/app/page.tsx
+++ manakin/app/page.tsx
@@ -1,17 +1,25 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Manakin</h1>
+    <main
+      className="flex flex-col items-center justify-between p-24"
+      style={{
+        minHeight: '350vh', // 3 times the screen height
+        backgroundImage: 'url(img/bg.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* <h1 className="text-4xl font-bold">Welcome to Manakin</h1> */}
       <div className="flex flex-col items-center">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Upload subject
+          Upload wooo
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Upload background
         </button>
       </div>
     </main>
-  );
+  )
 }
