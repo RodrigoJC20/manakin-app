c15dea6fab670f5e1fb9152cf23c2c7ca9cedd04
Pauline Wee <paulinekowee@gmail.com>
2024-06-01 12:26:25 -0400

    sfsf

--- manakin/app/page.tsx
+++ manakin/app/page.tsx
@@ -13,7 +13,16 @@ export default function Home() {
     >
       {/* <h1 className="text-4xl font-bold">Welcome to Manakin</h1> */}
       <div className="flex flex-col items-center">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          style={{
+            height: '260px', // 3 times the screen height
+            width: '800px', // 3 times the screen height
+            marginBottom: '20px',
+            backgroundImage: 'url(img/button1.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
           Upload wooo
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
