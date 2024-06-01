bdc8ef59540ae86df26ad9aa76855b2b2d5762af
Pauline Wee <paulinekowee@gmail.com>
2024-06-01 12:58:23 -0400

    changes

--- manakin/app/page.tsx
+++ manakin/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -50,9 +50,9 @@ export default function Home() {
 
       {/* <h1 className="text-4xl font-bold">Welcome to Manakin</h1> */}
       <div className="flex flex-col items-center">
-        <button
-          className="absolute left-0 button text-black text-xl"
-        >yes you</button>
+        <button className="absolute left-0 button text-black text-xl">
+          yes you
+        </button>
         <button
           className="button text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           style={{
@@ -80,8 +80,7 @@ export default function Home() {
           }}
         ></button>
       </div>
-      <div className="flex flex-col items-center">
-      </div>
+      <div className="flex flex-col items-center"></div>
     </main>
   )
 }
