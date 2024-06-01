efb5f51b82d197e6853cb2a09472ed4bbf2d3d57
Dhrumil Patel <dhrumilp15@gmail.com>
2024-06-01 13:32:37 -0400

    update server

--- server/app.py
+++ server/app.py
@@ -4,17 +4,23 @@ from typing import Dict
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import requests
-from server_secrets import REMOVEBG_API_KEY
-from server_secrets import GOOEY_API_KEY
+from server.server_secrets import REMOVEBG_API_KEY
+from server.server_secrets import GOOEY_API_KEY
+import os
 app = FastAPI()
 
 from removebg import RemoveBg
 
 rmbg = RemoveBg(REMOVEBG_API_KEY, "error.log")
 
+
+@app.get("/")
+async def root():
+	return {"Hello": "World"}
+
+
 @app.post("/create_file")
-async def create_upload_file(files: List[UploadFile] = File(...)):
-	foreground, background = files
+async def create_upload_file(foreground: UploadFile, background: UploadFile):
 	fg_name = foreground.filename
 	bg_name = background.filename
 	contents = foreground.file.read()
@@ -27,15 +33,16 @@ async def create_upload_file(files: List[UploadFile] = File(...)):
 	fg_name = fg_name + "_no_bg.png"
 	fg = cv2.imread(fg_name)
 	bg = cv2.imread(bg_name)
+	print(fg.shape, bg.shape)
 	row_length = bg.shape[0] // 4
 	col_length = bg.shape[1] // 4
 	dims = (bg.shape[0] // 4, bg.shape[1] // 4)
 
 	stretch_near = cv2.resize(fg, dims, interpolation=cv2.INTER_LINEAR)
-	center = (bg.shape[1] // 2, bg.shape[0] // 2)
-	row_slice = slice(center[0] - row_length // 2, center[0] + row_length // 2)
-	col_slice = slice(center[1] - col_length // 2, center[1] + col_length // 2)
-	bg[row_slice, col_slice] = stretch_near
+	center = (bg.shape[0] // 2, bg.shape[1] // 2)
+	row_slice = slice(center[0] - row_length, center[0] - row_length + dims[0])
+	col_slice = slice(center[1] - col_length, center[1] - col_length + dims[1])
+	bg[col_slice, row_slice] = stretch_near
 	cv2.imwrite(fg_name, fg)
 	cv2.imwrite(bg_name, bg)
 	return FileResponse(bg_name)
